1cf0d6e7d11b0d4711449cb736d65dccd735bdc3
bablubambal <bablubambal1999@gmail.com>
2024-03-21 23:52:48 +0530

    execption handling in js

---
+++ JS/exceptionhandling.js
@@ -0,0 +1,38 @@
+console.log('Exception Handling in JS')
+
+
+/**
+ * We will be using TRY Catch Block :
+ */
+
+a = 23;
+
+
+console.log(a)
+
+try {
+    // try your code ......
+
+    // console.log(c)
+
+    throw new EvalError("'User Exists'")
+
+
+}
+catch(e){
+    console.log(e)
+}
+finally{
+    console.log('Finally Block Executed Always : ')
+}
+
+// functio ram(){
+
+// }
+
+a = [1,2,3]
+console.log(a[10])
+// consoleee.log('giving eerro')
+
+console.log('a agian',a)
+
